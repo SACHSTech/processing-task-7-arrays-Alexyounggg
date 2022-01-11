ea6acfcff3a1a16963d2c2a4baadd39f53ea6f6c
Alexyounggg <alex.young23@ycdsbk12.ca>
2022-01-11 15:39:11 -0500

    Submit

--- Sketch.java
+++ Sketch.java
@@ -2,7 +2,13 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+	float[] snowY = new float [50];
+  float[] snowFallen = new float [400];
+  int [] snowMouseX = new int [25];
+  int [] snowMouseY = new int [25];
+  
+
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -16,21 +22,69 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(25, 25, 112);
+
+    for (int i = 0; i < snowFallen.length; i++){
+      snowFallen[i] = 400;
+    }
+    
+    for (int i = 0; i < snowY.length; i++){
+      snowY[i] = random(height);
+    }
+
+   
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    background(25, 25, 112);
+	  
+    for (int i = 0; i < snowY.length; i++){
+      float snowX = width * i/snowY.length;
+      noStroke();
+      ellipse (snowX, snowY[i], 5, 5);
+      
+
+      snowY[i] += 2;
+
+      if (keyPressed){
+        if (keyCode == UP){
+          snowY[i] -= 1;
+        }
+        else if (keyCode == DOWN){
+          snowY[i] += 3;
+        }
+      }
+      
+      for(int j = 0; j < snowFallen.length; j++){
+       fill(192, 192, 192);
+       rect (j, snowFallen[j], 5, height - snowFallen[j]);  
+       if (snowY[i] > height && j == snowX){
+         snowFallen[j]-= 2;
+       }
+     
+
+      }
+      if (snowY[i] > height){
+        snowY[i] = 0;       
+      }
+  
+    }
+    for (int k = 24; k > 0; k--){
+      snowMouseX[k] = snowMouseX[k - 1];
+      snowMouseY[k] = snowMouseY[k - 1];
+    }
+
+    snowMouseX[0] = mouseX;
+    snowMouseY[0] = mouseY;
+
+    for (int k = 0; k < snowMouseX.length; k++){
+      ellipse(snowMouseX[k], snowMouseY[k], 26 - k, 26 - k);
+    }
+    
   }
   
-  // define other methods down here.
 }
